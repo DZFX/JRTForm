85209cad120903b4a22765aad0a4974f9926a2a4
Isaac Delgado <danielidc.dz@gmail.com>
2017-10-12 16:18:34 -0500

    - Added TextView form file

---
+++ JRTForm-Swift/JRTForm-Swift/Podfiles/Fields/PhoneField/FormPhoneFieldTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  FormPhoneFieldTableViewCell.swift
+//  JRTForm-Swift
+//
+//  Created by Isaac Delgado on 10/12/17.
+//  Copyright © 2017 Isaac Delgado. All rights reserved.
+//
+
+import UIKit
+
+fileprivate let kFormTextFieldTableViewCell = "FormTextFieldTableViewCell"
+
+class FormPhoneFieldTableViewCell: FormTextFieldTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
